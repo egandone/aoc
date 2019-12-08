e485d2f431d44a6f872e800c47540489ab669c33
egandone <edone@sympatico.ca>
2019-12-08 12:11:06 -0500

    Completed day2, part 2

--- 2/day2.py
+++ 2/day2.py
@@ -1,4 +1,4 @@
-from collections import namedtuple
+from itertools import permutations
 
 
 def run_program(program_list):
@@ -21,8 +21,13 @@ def run_program(program_list):
     raise ValueError('Ran out of instructions before hitting 99')
 
 original_program = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,2,9,19,23,2,13,23,27,1,6,27,31,2,6,31,35,2,13,35,39,1,39,10,43,2,43,13,47,1,9,47,51,1,51,13,55,1,55,13,59,2,59,13,63,1,63,6,67,2,6,67,71,1,5,71,75,2,6,75,79,1,5,79,83,2,83,6,87,1,5,87,91,1,6,91,95,2,95,6,99,1,5,99,103,1,6,103,107,1,107,2,111,1,111,5,0,99,2,14,0,0]
-error_program = original_program
-error_program[1] = 12
-error_program[2] = 2
-output = run_program(error_program)
-print(output)
+
+for (noun, verb) in permutations(range(0,100), 2):
+    program = original_program.copy()
+    program[1] = noun
+    program[2] = verb
+    output = run_program(program)
+    if output[0] == 19690720:
+        print(f'{noun} {verb} --> {output[0]}')
+    
+
