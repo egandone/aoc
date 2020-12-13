fca94b33ac5f1efd715c706641bb0fd125288b76
egandone <edone@sympatico.ca>
2020-12-13 16:34:59 -0500

    Day 13 2020 - part 2

--- 2020/day13/day13_2f.py
+++ 2020/day13/day13_2f.py
@@ -1,4 +1,6 @@
 def find_first_hit(t0, dt, new_offset, new_delta):
+    # Just walk through time until the time at the
+    # offset is a whole multiple of the delta
     t = t0
     while True:
         if (t + new_offset) % new_delta == 0:
@@ -8,12 +10,9 @@ def find_first_hit(t0, dt, new_offset, new_delta):
 
 
 def find_second_hit(t0, dt, new_offset, new_delta):
-    t = t0 + dt
-    while True:
-        if (t + new_offset) % new_delta == 0:
-            break
-        t += dt
-    return t
+    # This just find the next hit after the current
+    # position (assumes the current is a hit)
+    return find_first_hit(t0 + dt, dt, new_offset, new_delta)
 
 
 bus_ids = []
@@ -28,16 +27,18 @@ with open("input.txt") as input:
 print(f'bus ids {bus_ids}')
 
 # Find the first t0 based on the first two entries
-t0 = find_first_hit(0, bus_ids[0][1], bus_ids[1][0], bus_ids[1][1])
-t1 = find_second_hit(t0, bus_ids[0][1], bus_ids[1][0], bus_ids[1][1])
-current_dt = t1 - t0
-for i in range(2, len(bus_ids)):
-    bus_id = bus_ids[i]
-    t0 = find_first_hit(t0, current_dt, bus_id[0], bus_id[1])
+t0 = 0
+current_dt = bus_ids[0][1]
+for i in range(0, len(bus_ids)-1):
+    current_bus = bus_ids[i]
+    next_bus = bus_ids[i+1]
+    # Find the next time the two buses hit the pattern
+    t0 = find_first_hit(t0, current_dt, next_bus[0], next_bus[1])
+    # ... and to compute the delta for the next iteration we
+    #     need to find the next time the buses hit
+    t1 = find_second_hit(t0, current_dt, next_bus[0], next_bus[1])
+    current_dt = t1 - t0
 
-    # Don't need to update current_dt if it's the last one
-    if (i < len(bus_ids) - 1):
-        t1 = find_second_hit(t0, current_dt, bus_id[0], bus_id[1])
-        current_dt = t1 - t0
-
-print(f'{t0}')
+# And then we just find the first hit with the last bus
+final_hit = find_first_hit(t0, current_dt, bus_ids[-1][0], bus_ids[-1][1])
+print(f'{final_hit}')
